abd5a0380365458e0644e77e575dba2f43b5dccf
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-17 13:24:54 -0800

    pass test to find instance of Order object

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -95,5 +95,23 @@ namespace VendorAndOrderTracker.Tests
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string title1 = "Test Order1 Title";
+      string description1 = "Test Order1 Description";
+      int price1 = 0;
+      string date1 = "Test Order1 Date";
+      string title2 = "Test Order2 Title";
+      string description2 = "Test Order2 Description";
+      int price2 = 0;
+      string date2 = "Test Order2 Date";
+      Order newOrder1 = new Order(title1, description1, price1, date1);
+      Order newOrder2 = new Order(title2, description2, price2, date2);
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
+    }
+
   }
 }
--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -31,10 +31,15 @@ namespace VendorAndOrderTracker.Models
     {
       _instances.Clear();
     }
-    
+
     public static List<Order> GetAll()
     {
       return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
